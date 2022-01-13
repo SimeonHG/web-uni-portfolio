d87a2fb5a94adac6b17878c786540b20580124bc
Shimo <simeonn.h.georgiev@gmail.com>
2022-01-13 23:10:30 +0200

    initial, but almost done :)

---
+++ app.js
@@ -0,0 +1,34 @@
+const hamburgerMenuButton = document.getElementById('hamburger-menu')
+const navList = document.getElementById('nav-list')
+
+function toggleButton() {
+	console.log("asdf")
+	navList.classList.toggle('show')
+}
+
+hamburgerMenuButton.addEventListener('click', toggleButton)
+
+function displayPopup(image) {
+	document.querySelector('.popup-img').style.display = 'block';
+	document.querySelector('.popup-img img').src = image.getAttribute('src');
+}
+
+function closePopup() {
+	document.querySelector('.popup-img').style.display = 'none';
+}
+
+
+const galleryImages = document.querySelectorAll('.gallery-img img')
+
+galleryImages.forEach(image =>{
+	image.onclick = () => {
+		displayPopup(image)
+	}
+});
+
+const x = document.querySelector('.popup-img span')
+x.onclick = () =>{ 
+	document.querySelector('.popup-img').style.display = 'none';
+}
+
+console.log(galleryImages)
